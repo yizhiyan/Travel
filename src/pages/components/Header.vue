12a863c9cc068fce3dbff52f393d0f1574b027e0
PC-20050101ZNAM\Administrator <464312406@qq.com>
2019-02-19 17:46:39 +0800

    更新

---
+++ src/pages/components/Header.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <div class="iconfont back-icon">&#xe624;</div>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe632;</span>
+      输入城市/景点/
+    </div>
+    <div class="header-right">
+      城市
+      <span class="iconfont arrow-icon">&#xe64a;</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home-header'
+}
+</script>
+
+<style scoped lang="stylus">
+@import "~styles/varibies.styl"
+.header
+  display:flex
+  background:$bgColor
+  height:.86rem;
+  line-height:.86rem
+  color:#fff
+  .header-left
+    width:.64rem
+    float:left
+    .back-icon
+      font-size:.4rem
+      text-align:center
+  .header-input
+    flex:1
+    background:#fff
+    border-radius:.1rem
+    margin-top:.12rem
+    margin-left:.2rem
+    height:.64rem
+    line-height:.64rem
+    color: #ccc
+    padding-left:.2rem
+  .header-right
+    width:1.24rem
+    float: right
+    text-align:center
+    .arrow-icon
+      font-size:.18rem
+</style>
---
+++ src/pages/components/Swiper.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption" ref="mySwiper" >
+      <!-- slides -->
+      <swiper-slide>
+        <img src="http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20191/3921acb51460dc6fa9e3c7464fac5cbb.jpg_750x200_d992573a.jpg" alt="">
+      </swiper-slide>
+      <swiper-slide>
+        <img src="http://img1.qunarzz.com/piao/fusion/1812/d6/daa880b254940402.jpg_750x200_b114308a.jpg" alt="">
+      </swiper-slide>
+      <swiper-slide>
+        <img src="http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20191/a211765c0bb579b18c6e7d6d75997a48.jpg_750x200_b6921fa6.jpg" alt="">
+      </swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination"  slot="pagination"></div>
+      <div class="swiper-scrollbar"   slot="scrollbar"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home-swiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      }
+    }
+  }
+}
+</script>
+
+<style scoped lang="stylus">
+  .wrapper >>> .swiper-pagination-bullet-active
+    background #fff
+  .wrapper
+    width:100%;
+    height:0;
+    overflow:hidden;
+    padding-bottom:31.25%;
+    img
+      width:100%;
+</style>
