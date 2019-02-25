9240c277bcc35f1a02704b6ae084f6e2bd656af7
PC-20050101ZNAM\Administrator <464312406@qq.com>
2019-02-25 17:37:11 +0800

    创建底部

---
+++ src/pages/components/Footer.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="footer-container">
+    <ul class="footer-links">
+      <li>
+        <div class="icon"></div>
+        <a href="javascript:;">机票</a>
+      </li>
+      <li>
+        <div class="icon"></div>
+        <a href="javascript:;">机票</a>
+      </li>
+      <li>
+        <div class="icon"></div>
+        <a href="javascript:;">机票</a>
+      </li>
+      <li>
+        <div class="icon"></div>
+        <a href="javascript:;">机票</a>
+      </li>
+      <li>
+        <div class="icon"></div>
+        <a href="javascript:;">机票</a>
+      </li>
+      <li>
+        <div class="icon"></div>
+        <a href="javascript:;">机票</a>
+      </li>
+      <li>
+        <div class="icon"></div>
+        <a href="javascript:;">机票</a>
+      </li>
+      <li>
+        <div class="icon"></div>
+        <a href="javascript:;">机票</a>
+      </li>
+      <li>
+        <div class="icon"></div>
+        <a href="javascript:;">机票</a>
+      </li>
+      <li>
+        <div class="icon"></div>
+        <a href="javascript:;">机票</a>
+      </li>
+      <li>
+        <div class="icon"></div>
+        <a href="javascript:;">机票</a>
+      </li>
+      <li>
+        <div class="icon"></div>
+        <a href="javascript:;">机票</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home-footer'
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -6,6 +6,7 @@
     <home-popular></home-popular>
     <home-guess></home-guess>
     <home-weekend></home-weekend>
+    <home-footer></home-footer>
   </div>
 </template>
 
@@ -15,7 +16,8 @@ import HomeSwiper from './components/Swiper'
 import HomeIcons from './components/Icon'
 import HomePopular from './components/Popular'
 import HomeGuess from './components/Guess'
-import HomeWeekend from './components/weekend'
+import HomeWeekend from './components/Weekend'
+import HomeFooter from './components/Footer'
 
 export default {
   name: 'home',
@@ -25,7 +27,8 @@ export default {
     HomeIcons,
     HomePopular,
     HomeGuess,
-    HomeWeekend
+    HomeWeekend,
+    HomeFooter
   }
 }
 </script>
